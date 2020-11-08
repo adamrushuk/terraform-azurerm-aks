3a4d80e575ea1d4ebc84bf887883f638086f41a4
Adam Rush <adamrushuk@gmail.com>
2020-11-08 14:51:14 +0000

    Removed old providers

--- versions.tf
+++ versions.tf
@@ -1,10 +1,6 @@
 terraform {
   # versioning syntax: https://www.terraform.io/docs/configuration/version-constraints.html#version-constraint-syntax
   required_version = ">= 0.12"
-  required_providers {
-    helm       = ">= 1.3.2"
-    kubernetes = ">= 1.13.2"
-  }
 }
 
 # https://github.com/terraform-providers/terraform-provider-azurerm/releases
