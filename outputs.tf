3f2ab134a3af09fd347e8a3cb82504f3e5653cfb
Adam Rush <adamrushuk@gmail.com>
2020-11-08 17:55:31 +0000

    Added aks cluster name output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "name" {
+  description = "Name of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks.node_resource_group
+}
+
 output "node_resource_group" {
   description = "The name of the Resource Group where the Kubernetes Nodes should exist"
   value       = azurerm_kubernetes_cluster.aks.node_resource_group
@@ -15,7 +20,7 @@ output "kube_config_raw" {
 }
 
 output "kube_config" {
-  description = "Kube configuration of AKS Cluster"
+  description = "Kube configuration of AKS cluster"
   value       = azurerm_kubernetes_cluster.aks.kube_config
   sensitive   = true
 }
