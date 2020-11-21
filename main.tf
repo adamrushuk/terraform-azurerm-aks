846f14e30f2c56a2032abe639453062e0da0fc36
Adam Rush <adamrushuk@gmail.com>
2020-11-21 10:07:58 +0000

    Changed to one consolidated main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,48 @@
+terraform {
+  # versioning syntax: https://www.terraform.io/docs/configuration/version-constraints.html#version-constraint-syntax
+  required_version = ">= 0.12"
+
+  # providers (pin all versions)
+  # versioning syntax: https://www.terraform.io/docs/configuration/modules.html#module-versions
+  # ~> 1.0 = 1.x
+  required_providers {
+    # https://github.com/terraform-providers/terraform-provider-azuread/releases
+    azuread = "~> 1.0"
+    random  = "~> 2.2"
+    tls     = "~> 2.1"
+  }
+}
+
+# https://github.com/terraform-providers/terraform-provider-azurerm/releases
+provider "azurerm" {
+  version = "~> 2.20"
+  features {}
+}
+
+locals {
+  # TODO: consider moving defaults to object var, as per: https://binx.io/blog/2020/01/02/module-parameter-defaults-with-the-terraform-object-type/
+  default_agent_profile = {
+    name                  = "default"
+    count                 = 1
+    orchestrator_version  = var.kubernetes_version
+    vm_size               = "Standard_D2s_v3"
+    os_type               = "Linux"
+    availability_zones    = [1, 2, 3]
+    enable_auto_scaling   = false
+    min_count             = null
+    max_count             = null
+    type                  = "VirtualMachineScaleSets"
+    node_taints           = null
+    # TODO: add custom vnet support
+    # vnet_subnet_id        = var.nodes_subnet_id
+    max_pods              = 30
+    os_disk_size_gb       = 32
+    enable_node_public_ip = false
+  }
+
+  default_node_pool = merge(local.default_agent_profile, var.default_node_pool)
+}
+
 # AKS
 resource "tls_private_key" "ssh" {
   algorithm = "RSA"
